231b1cf93ddfe69e2ae7b70518952778061b17f3
Diego Lazanha <diego_lazanha01@hotmail.com>
2021-04-02 09:36:59 -0300

    Initial Commit

---
+++ GuessTheFlag2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheFlag2
+//
+//  Created by Diego Araujo Lazanha on 02/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
