41294e07ab52d50236ca9a6e81dd58022312554d
dlazanha <78104143+dlazanha@users.noreply.github.com>
2021-05-07 16:51:18 -0300

    Add files via upload

---
+++ AppLifeCircle/AppLifeCircle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCircle
+//
+//  Created by Diego Araujo Lazanha on 10/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
